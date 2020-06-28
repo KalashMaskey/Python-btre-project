4d752add7da7db2be01fb2b3314ce6a5df906938
kalash <kalashm24@gmail.com>
2020-06-28 17:24:33 +0545

    Git Ignore,Static Settings,Templates

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\01.essential\tutorial\tutorials-work\python-btre-project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
