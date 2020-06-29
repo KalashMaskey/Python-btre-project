0eb1d1aeb5694a211030c62a889a05524558078d
kalash <kalashm24@gmail.com>
2020-06-29 17:15:06 +0545

    Adding Data

--- realtors/models.py
+++ realtors/models.py
@@ -6,7 +6,7 @@ class Realtor(models.Model):
     photo       = models.ImageField(upload_to='photos/%Y/%m/%d/')
     description = models.TextField(blank=True)
     phone       = models.CharField(max_length=128)
-    email       = models.CharField(max_length=20)
+    email       = models.CharField(max_length=30)
     is_mvp      = models.BooleanField(default=False)
     hire_date   = models.DateTimeField(default=datetime.now, blank=True)
 
