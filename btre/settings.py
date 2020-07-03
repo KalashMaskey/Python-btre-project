18e31d4c0ba638b5822d7fb97b3b909700fc4b7b
kalash <kalashm24@gmail.com>
2020-07-03 13:33:16 +0545

    Inquiry check and email function

--- btre/settings.py
+++ btre/settings.py
@@ -142,3 +142,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+#Email Config
+EMAIL_HOST = 'mail.supplybox.com.np'
+EMAIL_PORT = 26
+EMAIL_HOST_USER = 'kalash.maskey@supplybox.com.np'
+EMAIL_HOST_PASSWORD = 'supplybox@123'
+EMAIL_USE_TLS=True
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -13,6 +14,13 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        # Inquiry Already Made
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request,'You have already made an inquirt for this listing')
+                return redirect('/listings/'+listing_id)
         contact = Contact(
             listing=listing,
             listing_id=listing_id,
@@ -25,6 +33,16 @@ def contact(request):
 
         contact.save()
 
-        messages.success(request,
-            ' Your request has been submitted, a realtor will get back to you soon')
+        #Send mail
+        send_mail(
+            'property Listing Inquiry',
+            'There has been an inquiry for' + listing + '. Sign into the admin panel for more info',
+            'kalashm24@gmail.com',
+            [realtor_email, 'kalashm24@gmail.com'],
+            fail_silently=False,
+        )
+
+
+
+        messages.success(request,' Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
